8e4fda924f8e622de41d688501d7bd4d37203baf
madjid yacini <madjid.yacini@gmail.com>
2018-03-27 19:46:12 +0200

    some controls with mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,13 +6,27 @@ const Product = require("../models/product")
 // GET METHOD OF A PRODUCTS
 router.get('/', function(req , res ,next) {
      Product.find()
+     .select(" name price _id ")
      .exec()
      .then(docs =>{
-        console.log("from database"+ docs)
-        console.log(docs.length)
+        const response = {
+            count : docs.length,
+            products : docs.map(doc =>{
+                return {
+                    name : doc.name,
+                    price : doc.price,
+                    _id : doc. _id,
+                    request :{
+                        type :"GET",
+                        url :"http://localhost:3000/products/"+doc. _id
+                    }
+                }
+            }) 
+        }
+        
         if(docs.length >0){
          res.status(200).json({
-             document : docs
+             document : response
          })
         }else{
             res.status(200).json({
@@ -59,7 +73,7 @@ router.get('/:productId',function(req,res,next){
    .exec()
    .then(doc =>{
        if (doc){
-       console.log("form database",doc)
+      
        res.status(200).json({doc })
        }else{
            res.status(404).json({message: "no valid entry found for provided ID"})
